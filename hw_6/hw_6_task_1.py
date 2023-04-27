4f5302208d56b8339c251155ba5cf64b358c5e00
DanielFalcony <DanielFalcony@gmail.com>
2023-04-27 17:33:08 +0300

    hw_5 task_1 done

--- hw_6/hw_6_task_1.py
+++ hw_6/hw_6_task_1.py
@@ -5,6 +5,6 @@ from modules import *
 if __name__ == '__main__':
     date = sys.argv[1] if len(sys.argv) > 1 else input('Введите дату вручную: ')
     if calendar(date):
-        print("Дата верна")
+        print(calendar(date))
     else:
         print('Дата не верна')
